d624f52c285a17a7ae97661a99ecedd9f9368fcb
jungwon <thswjd6@gmail.com>
2024-10-02 18:10:39 +0900

    bookmark

--- brochure/src/components/favorites/Favorite.js
+++ brochure/src/components/favorites/Favorite.js
@@ -1,19 +1,23 @@
 
 import {useState,useEffect} from "react";
 import { getFavoList } from "../../Api/BrochureApi";
+import { useNavigate } from "react-router-dom";
 export const Favorite = () => {
-  
+    const navigate = useNavigate();
     const [favoList,setFavoList] = useState([]);
-
+ 
     useEffect(()=>{
         setFavoList(getFavoList);
     },[]);
   return(
+    <>
     <div>
+    <button onClick={()=> navigate(-1)}>돌아가기</button>
         {favoList.map((item, index) => (
                 <div key={index}> 
                 <li>{item.name} {item.place} </li>
                 </div>
             ))} 
     </div>
+    </>
   )};
