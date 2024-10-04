352f8337e9cf9bcc1f7d7edbf2d739fd39cc4d68
nicdkim <zzsehdrb@naver.com>
2024-10-04 18:04:42 +0900

    KakaoLogin access / NaverLogin modi

--- brochure/src/components/auth/NaverLogin.js
+++ brochure/src/components/auth/NaverLogin.js
@@ -4,36 +4,20 @@
 */
 
 import React, { useEffect } from 'react';
-import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
 
 const NaverLogin = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
     const naverLogin = new window.naver.LoginWithNaverId({
       clientId: process.env.REACT_APP_NAVER_CLIENT_ID,
       callbackUrl: 'http://localhost:3000/oauth/naver/callback',
-      isPopup: false,  // 팝업 방식으로 로그인 할 것인지
-      loginButton: { color: 'green', type: 3, height: 48 },
+      isPopup: false, // 팝업 형태로 로그인 진행 여부
+      loginButton: { color: 'green', type: 3, height: 50 }, // 네이버에서 제공하는 버튼 스타일
     });
-    naverLogin.init();
 
-    // 로그인 콜백 함수 설정
-    window.addEventListener('load', function () {
-      naverLogin.getLoginStatus((status) => {
-        if (status) {
-          const token = naverLogin.accessToken.accessToken;
-          Cookies.set('token', token, { expires: 1 });  // 로그인 성공 시 쿠키에 토큰 저장
-          navigate('/');  // 메인 페이지로 이동
-        }
-      });
-    });
-  }, [navigate]);
+    naverLogin.init(); // 네이버 로그인 초기화
+  }, []);
 
-  return (
-    <div id="naverIdLogin" />
-  );
+  return <div id="naverIdLogin" />;
 };
 
 export default NaverLogin;
