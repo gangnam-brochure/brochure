3c65299133d460ddf8be8ecf882487383a3c8887
yzzzang <yz1211ch@gmail.com>
2024-10-04 14:23:05 +0900

    msg

--- brochure/src/App.js
+++ brochure/src/App.js
@@ -8,8 +8,11 @@ import MainPage from './layout/MainPage';
 import MyPage from './components/mypage/MyPage';
 import LoginRedirectHandler from './components/auth/LoginRedirectHandler';  // 리다이렉트 핸들러
 import Logout from './components/auth/Logout';
+import FavoritePage from './layout/FavoritePage';
+import PlaceTest from './components/favorites/PlaceTest';
 import PlaceDetail from './components/placelist/PlaceDetail';
 import PlaceListNearby from './components/placelist/PlaceListNearby';
+
 
 function App() {
   return (
@@ -20,10 +23,15 @@ function App() {
         </Route>  {/* 메인 페이지와 리다이렉트 처리 */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />  {/* 로그인 페이지 */}
-        <Route path="/logout" element={<Logout />} /> {/* 로그아웃 페이지 */}
+        <Route path= "/favorites" element={<FavoritePage/>}/>
+        <Route path="/test" element={<PlaceTest/>}/>
         <Route path="/sociallogin" element={<SocialLoginWrapper />} />
         <Route path="/profile" element={<PrivateRoute />} />  {/* 프로필 페이지 */}
         <Route path="/mypage" element={<MyPage />} />  {/* 마이 페이지 */}
+
+        {/* 카카오 및 네이버 로그인 콜백 처리 */}
+        <Route path="/oauth/kakao/callback" element={<LoginRedirectHandler />} />
+        <Route path="/oauth/naver/callback" element={<LoginRedirectHandler />} />
       </Routes>
     </Router>
   );
